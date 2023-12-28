8271af32ba22fcd1fc932fa8b419036635512ceb
LeticiaCardosoCarmo <lcs.leticia@hotmail.com>
2023-12-28 11:56:15 -0300

    Desafio Smartphone concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone IPhone");
+Smartphone iphone = new Iphone("12345", "IPhone 13", "7410852963", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("CapCut");
+
+Console.WriteLine("Smartphone IPhone");
+Smartphone nokia = new Nokia("6789", "G11", "7410852963", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tiktok");
